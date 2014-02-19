eb68ef62142650b87d7c8a3d3bb6d3056c6a0dd5
Sebastian Widmer <sebastian.widmer@terreactive.ch>
2014-02-19 11:11:31 +0100

    build

--- dist/setZeroTimeout.js
+++ dist/setZeroTimeout.js
@@ -1,42 +1,18 @@
-var setZeroTimeout;
-
-setZeroTimeout = new function() {
-  var counter, handleMessage, messageName, timeouts, zeroTimeoutsCount;
+this.setZeroTimeout || (this.setZeroTimeout = new function() {
+  var handleMessage, messageName, timeouts;
   timeouts = [];
   messageName = "zero-timeout-message-" + new Date().getTime();
-  zeroTimeoutsCount = 1000;
-  counter = 0;
   handleMessage = function(event) {
-    var fn;
     if (event.data === messageName) {
-      event.cancelBubble = true;
-      event.returnValue = false;
-      if (typeof event.stopPropagation === "function") {
-        event.stopPropagation();
-      }
-      if (typeof event.preventDefault === "function") {
-        event.preventDefault();
-      }
-      if (0 < timeouts.length) {
-        fn = timeouts.shift();
-        return fn();
+      event.stopPropagation();
+      if (timeouts.length) {
+        return timeouts.shift()();
       }
     }
   };
-  if (window.addEventListener) {
-    window.addEventListener("message", handleMessage, true);
-  } else if (window.attachEvent) {
-    zeroTimeoutsCount = 0;
-    window.attachEvent("onmessage", handleMessage);
-  }
+  window.addEventListener("message", handleMessage, true);
   return function(fn) {
-    counter--;
-    if (counter < 0) {
-      counter = zeroTimeoutsCount;
-      return setTimeout(fn, 10);
-    } else {
-      timeouts.push(fn);
-      return window.postMessage(messageName, "*");
-    }
+    timeouts.push(fn);
+    return window.postMessage(messageName, "*");
   };
-};
+});
